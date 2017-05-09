64f4603f46bed7c85315c5d5a98d227c2671a3c9
olli133 <Ibs%2016bGHa>
2017-05-09 18:31:00 +0200

    Projektdateien hinzufügen.

---
+++ SDDP1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDDP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
